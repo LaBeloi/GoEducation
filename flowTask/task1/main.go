7b30c7828eff02c9689c291c8d6dbf97dc26eb88
LaBeloi <dovgij.vladkoko@gmail.com>
2022-06-23 13:21:24 +0300

    transport logic to main func

--- flowTask/task1/main.go
+++ flowTask/task1/main.go
@@ -4,30 +4,26 @@ import (
 	"fmt"
 )
 
-func deleteDublicates(slice []int) []int {
-	if len(slice) == 1 {
-		return slice
-	}
-
-	mapOfIndexes := make(map[int]bool)
-	sliceOfItems := []int{}
-
-	for _, item := range slice {
-		_, ok := mapOfIndexes[item]
-		if !ok {
-			mapOfIndexes[item] = true
-			sliceOfItems = append(sliceOfItems, item)
-		}
-	}
-
-	return sliceOfItems
-}
-
 func main() {
-	arr := []int{4, 1, 4, -4, 6, 3, 8, 8}
+	arr := []int{4, 1, 4, 4, -4, 6, 3, 8, 8}
 	var result []int
 
-	result = deleteDublicates(arr)
+	if len(arr) == 1 {
+		result = arr
+	} else {
+		mapOfIndexes := make(map[int]bool)
+		sliceOfItems := []int{}
+
+		for _, item := range arr {
+			_, ok := mapOfIndexes[item]
+			if !ok {
+				mapOfIndexes[item] = true
+				sliceOfItems = append(sliceOfItems, item)
+			}
+		}
+
+		result = sliceOfItems
+	}
 
 	fmt.Println(result)
 }
